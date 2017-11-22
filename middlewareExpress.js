f5ac43f896a6abd3196dcbf5b571ef9aa52b774f
Edgar Castillo <3dg1992@gmail.com>
2017-11-22 16:07:18 -0600

    actualizando repositorio

--- middlewareExpress.js
+++ middlewareExpress.js
@@ -1,17 +1,24 @@
 'use strict'
 var express = require('express'),
     app = express(),
-    cookieParser =  require('cookie-parser'),
+    cookieParser = require('cookie-parser'),
     cookieSession = require('cookie-session')
 
-app
-    .use(cookieParser() )
-    .use(cookieSession({secret: "secreto"}) )    
 
 app
-    .get('/',(req,res)=>{
-        res.end('<h1>HOLA</h1>')
+// .use(cookieParser())
+    .use(cookieSession({ secret: "secreto" }))
+
+.get('/', (req, res) => {
+
+        req.session.visitas || (req.session.visitas = 0)
+
+        let n = req.session.visitas++
+            res.end(`<h1>HOLA ERES LA VISITA NUMERO ${n} </h1>`)
     })
-    .listen(3000)
+    .get('/usuario/:id-:nombre-:edad', (req, res) => {
+        res.end(`<h1>hola</h1>`)
+    })
+    .listen(4000)
 
-    console.log('up server 3000')
+console.log('up server 4000')
